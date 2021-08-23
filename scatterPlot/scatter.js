25255d34faff568501eefca712345501ed6dd433
Grady CLARK <grady.clark@students.emmauscc.sa.edu.au>
2021-08-23 14:22:11 +0930

    changed csv for array and created scatter plot

--- scatterPlot/scatter.js
+++ scatterPlot/scatter.js
@@ -1,44 +1,61 @@
-// set the dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 30, left: 60},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+var dataset1 = [[10, 200], [20, 140], [30, 100], [40, 60], [50, 40], [60, 38], [70, 34], [80, 30], [90, 28]];
 
 // append the svg object to the body of the page
-var svg = d3.select("#my_dataviz")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+var svg = d3.select("svg"),
+  margin = 200,
+  width = svg.attr("width") - margin,
+  height = svg.attr("height") - margin
 
-//Read the data
-d3.csv("example.csv", function(data) {
+var xScale = d3.scaleLinear().domain([0, 100]).range([0, width]),
+  yScale = d3.scaleLinear().domain([0, 200]).range([height, 0]);
 
-  // Add X axis
-  var x = d3.scaleLinear()
-    .domain([0, 4000])
-    .range([ 0, width ]);
+  var g = svg.append("g")
+            .attr("transform", "translate(" + 100 + "," + 100 + ")");
+
+  // Title
+  svg.append('text')
+  .attr('x', width/2 + 100)
+  .attr('y', 100)
+  .attr('text-anchor', 'middle')
+  .style('font-family', 'Helvetica')
+  .style('font-size', 20)
+  .text('Scatter Plot');
+  
+  // X label
+  svg.append('text')
+  .attr('x', width/2 + 100)
+  .attr('y', height - 15 + 150)
+  .attr('text-anchor', 'middle')
+  .style('font-family', 'Helvetica')
+  .style('font-size', 12)
+  .text('Independant');
+  
+  // Y label
+  svg.append('text')
+  .attr('text-anchor', 'middle')
+  .attr('transform', 'translate(60,' + height + ')rotate(-90)')
+  .style('font-family', 'Helvetica')
+  .style('font-size', 12)
+  .text('Dependant');
+
+  //X Axis
+  g.append("g")
+         .attr("transform", "translate(0," + height + ")")
+         .call(d3.axisBottom(xScale));
+        
+  //Y Axis
+  g.append("g")
+    .call(d3.axisLeft(yScale));
+
   svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
-
-  // Add Y axis
-  var y = d3.scaleLinear()
-    .domain([0, 500000])
-    .range([ height, 0]);
-  svg.append("g")
-    .call(d3.axisLeft(y));
-
-  // Add dots
-  svg.append('g')
     .selectAll("dot")
-    .data(data)
+    .data(dataset1)
     .enter()
     .append("circle")
-      .attr("cx", function (d) { return x(d.GrLivArea); } )
-      .attr("cy", function (d) { return y(d.SalePrice); } )
-      .attr("r", 1.5)
-      .style("fill", "#69b3a2")
+    .attr("cx", function (d) { return xScale(d[0]); } )
+    .attr("cy", function (d) { return yScale(d[1]); } )
+    .attr("r", 2)
+    .attr("transform", "translate(" + 100 + "," + 100 + ")")
+    .style("fill", "#CC0000");
 
-})
+
