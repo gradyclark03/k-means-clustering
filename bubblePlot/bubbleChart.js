b2a7fe7ffe480627138306d7d5b916c4eb71d4d5
Grady CLARK <grady.clark@students.emmauscc.sa.edu.au>
2021-09-21 12:42:39 +0930

    working bubble chart not integrated with data

---
+++ bubblePlot/bubbleChart.js
@@ -0,0 +1,34 @@
+// Step 1
+var data = [
+  {cluster:"Cluster 1", x: 50, y: 40, val: 10, color: "#1e81b0"},
+  {cluster:"Cluster 2", x: 30, y: 120, val: 30, color: "#e28743"},
+  {cluster:"Cluster 3", x: 120, y: 80, val: 48, color: "#ff0000"},
+]
+
+// Step 3
+var svg = d3.select("svg")
+          .attr("width", 500)
+          .attr("height", 500);
+
+// Step 4
+svg.selectAll("circle")
+  .data(data).enter()
+  .append("circle")
+  .attr("cx", function(d) {return d.x})
+  .attr("cy", function(d) {return d.y})
+  .attr("r", function(d) {
+    return Math.sqrt((d.val)*100)/Math.PI 
+  })
+  .attr("fill", function(d) {
+    return d.color;
+  });
+
+// Step 5
+svg.selectAll("text")
+  .data(data).enter()
+  .append("text")
+  .attr("x", function(d) {return d.x+(Math.sqrt(d.val)/Math.PI)+20})
+  .attr("y", function(d) {return d.y+4})
+  .text(function(d) {return d.cluster})
+  .style("font-family", "arial")
+  .style("font-size", "12px")
